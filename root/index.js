1353c6de8062f4d378eb0005a2b5c26419196e23
N3u7r0 <brian_sabatini1993@hotmail.com>
2022-07-04 12:31:34 -0300

    commit inicial

---
+++ root/index.js
@@ -0,0 +1,4 @@
+console.log("JavaScript-04. BrianSabatini - CoderHouse");
+
+let NombreApellido = prompt("Bienvenido! ingrese su Nombre y Apellido");
+let ItemUsuario = prompt("Hola " + NombreApellido + ". ¿que esta buscando, buzo o campera?.");
