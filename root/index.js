f53e2e0460b27f3592a12f0f010b5cc827698f18
N3u7r0 <brian_sabatini1993@hotmail.com>
2022-07-05 17:23:15 -0300

    faltan bucles en consolelog para ahorrar en linea de codigo

--- root/index.js
+++ root/index.js
@@ -31,24 +31,37 @@ console.log(camp3);
 //--------------------------------------------------------------
 //front dinamica.
 let userName = prompt("Bienvenido! ingrese su Nombre");
-let selecUsuario = prompt("Hola " + userName + ". ¿Que producto va a ingresar, buzo o campera?.");
+console.log(userName+" ingreso a la app");
+let selecUsuario = prompt("Hola " + userName + ". ¿Que producto vas a ingresar, buzo o campera?.");
 if (selecUsuario === "buzo") {
     let marcaIn = prompt("ingrese la marca:");
     let colorIn = prompt("ingrese el color:");
-    let talleIn = prompt("ingrese el talle:");
-    let precioIn = prompt("ingrese el precio:");
+    let talleIn =  prompt("ingrese el talle: -XS-S-M-L-X-XL-XXL-:");
+    let precioIn = prompt("ingrese el precio -sin el signo $-:")
+    // nueva entrada.
+    const buzo4 = new Buzo(marcaIn, colorIn, talleIn, precioIn);
+    console.log(userName+"ingreso un nuevo item");
     console.log(buzo4);
 }
 else if (selecUsuario === "campera") {
     let marcaIn2 = prompt("ingrese la marca:");
     let colorIn2 = prompt("ingrese el color:");
-    let talleIn2 = prompt("ingrese el talle:");
-    let precioIn2 = prompt("ingrese el precio:");
+    let talleIn2 = prompt("ingrese el talle: -XS-S-M-L-X-XL-XXL-:");
+    let precioIn2 = prompt("ingrese el precio -sin el signo $-:");
+    // nueva entrada.
+    const camp4 = new Campera(marcaIn2, colorIn2 ,talleIn2, precioIn2);
     console.log(camp4);
 }
-// nueva entrada.
-const buzo4 = new Buzo(marcaIn, ColorIn, talleIn, precioIn);
-const camp4 = new Campera(marcaIn2, ColorIn2 ,talleIn2, precioIn2);
+alert ("Operacion realizada con exito.")
+alert(userName+"!!! Gracias por ser parte de nuestro equipo!. Vamos a realizar un sorteo sorpresa el 21/04 para todos los colaboradores, y queremos que tengas tu numero!.")
+for (let i = 1; i < 150; i++) {
+    alert ("!!!"+userName + " tu numero de sorteo es el numero "+i+"!!!")
+    break
+
+}
+
+
+
 
 
 
