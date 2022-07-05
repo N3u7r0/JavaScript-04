7db4ed73500fe5b947050da1ee810f30fdfbd901
N3u7r0 <brian_sabatini1993@hotmail.com>
2022-07-05 20:19:09 -0300

    mejoras y documento comentado

--- root/index.js
+++ root/index.js
@@ -6,11 +6,14 @@ function Buzo(marca, color, talle, precio) {
     this.talle = talle;
     this.precio = precio;
 }
-function Campera(marca, color, talle, precio) {
-    this.marca = marca;
-    this.color = color;
-    this.talle = talle;
-    this.precio = precio;
+//constructor con class.
+class Campera {
+    constructor(marca, color, talle, precio) {
+        this.marca = marca;
+        this.color = color;
+        this.talle = talle;
+        this.precio = precio;
+    }
 }
 // items creados.
 const buzo1 = new Buzo("Rush", "Negro", "S", 2000);
@@ -27,37 +30,41 @@ console.log("Camperas guardadas");
 console.log(camp1);
 console.log(camp2);
 console.log(camp3);
+//----------------------------------------
 
-//--------------------------------------------------------------
 //front dinamica.
 let userName = prompt("Bienvenido! ingrese su Nombre");
-console.log(userName+" ingreso a la app");
+console.log(userName + " ingreso a la app");
 let selecUsuario = prompt("Hola " + userName + ". ¿Que producto vas a ingresar, buzo o campera?.");
+//ingreso de articulo tipo buzo con array.
 if (selecUsuario === "buzo") {
     let marcaIn = prompt("ingrese la marca:");
     let colorIn = prompt("ingrese el color:");
-    let talleIn =  prompt("ingrese el talle: -XS-S-M-L-X-XL-XXL-:");
+    let talleIn = prompt("ingrese el talle: -XS-S-M-L-X-XL-XXL-:");
     let precioIn = prompt("ingrese el precio -sin el signo $-:")
-    // nueva entrada.
-    const buzo4 = new Buzo(marcaIn, colorIn, talleIn, precioIn);
-    console.log(userName+"ingreso un nuevo item");
-    console.log(buzo4);
+    //array
+    let buzoArr = [precioIn, talleIn, colorIn, marcaIn];
+    buzoArr.reverse();//meti esto aca como para dar por entendido esta herramienta. saludos!
+    console.log(userName + "ingreso un nuevo item");
+    console.log(buzoArr);
 }
+//ingreso de articulo tipo campera
 else if (selecUsuario === "campera") {
     let marcaIn2 = prompt("ingrese la marca:");
     let colorIn2 = prompt("ingrese el color:");
     let talleIn2 = prompt("ingrese el talle: -XS-S-M-L-X-XL-XXL-:");
     let precioIn2 = prompt("ingrese el precio -sin el signo $-:");
-    // nueva entrada.
-    const camp4 = new Campera(marcaIn2, colorIn2 ,talleIn2, precioIn2);
-    console.log(camp4);
+    // otro array.
+    let campArr = [marcaIn2, colorIn2, talleIn2, precioIn2];
+    console.log(userName + "ingreso un nuevo item");
+    console.log(campArr);
 }
-alert ("Operacion realizada con exito.")
-alert(userName+"!!! Gracias por ser parte de nuestro equipo!. Vamos a realizar un sorteo sorpresa el 21/04 para todos los colaboradores, y queremos que tengas tu numero!.")
+alert("Operacion realizada con exito.")
+alert(userName + "!!! Gracias por ser parte de nuestro equipo!. Vamos a realizar un sorteo sorpresa el 21/04 para todos los colaboradores, y queremos que tengas tu numero!.")
 for (let i = 1; i < 150; i++) {
-    alert ("!!!"+userName + " tu numero de sorteo es el numero "+i+"!!!")
-    break
-
+    alert("!!!" + userName + " tu numero de sorteo es el numero " + i + "!!!")
+    console.log(userName + " recibio el numero " + i);
+    break;
 }
 
 
